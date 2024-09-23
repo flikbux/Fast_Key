56ee60fa170915b0e6d84fd3e989aa9356c4375b
Bienvenu Elie <106546022+flikbux@users.noreply.github.com>
2024-09-23 15:57:35 +0200

    camera marent

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import numpy as np
-import cv2
+import cv2 as cv
+from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(0)
+imgL = cv.imread('tsukuba_l.png', cv.IMREAD_GRAYSCALE)
+imgR = cv.imread('tsukuba_r.png', cv.IMREAD_GRAYSCALE)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    cv2.imshow('gray',gray)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+stereo = cv.StereoBM.create(numDisparities=16, blockSize=15)
+disparity = stereo.compute(imgL, imgR)
+plt.imshow(disparity, 'gray')
+plt.show()
